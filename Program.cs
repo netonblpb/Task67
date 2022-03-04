6a8387ce3d35ff94ff5cb079d19075b66307307f
netonblpb <skullbog@gmail.com>
2022-03-04 16:50:31 +0300

    Task67 Complete

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+// N = 5. -> "5, 4, 3, 2, 1"
+
+Console.Clear();
+
+Console.Write("Задайте N: ");
+int num = int.Parse(Console.ReadLine());
+
+void show(int n)
+{
+    if (n > 0)
+    {
+        Console.Write(n + "   ");
+        show(n - 1);        
+    }
+}
+show(num);
